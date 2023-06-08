8a285df977ac0938ab39ad18b6ab4347df5b9fd0
Emily-Bautista <112963205+Emily-Bautista@users.noreply.github.com>
2023-06-08 11:13:34 -0600

    Lista de Hobies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const nombre = "Emily Bautista Sosa"; "nombre";
+  return ( 
+  <div> 
+  <h1>Lista de hobbies de {nombre}</h1>
+  <ol>
+  <li>Jugar LOL</li>
+  <li>Jugar Max-Payne</li>
+  <li>Leer Fanfics</li>
+  <li>Jugar Mobile Legends</li>
+  <li>Ver Anime</li>
+  </ol>
+  </div>
+  )
+}
+
+export default App
